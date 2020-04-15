9df22b6180963c54a27609e28ed55ec773b630a1
mathieu.brunot <mathieu.brunot@monogramm.io>
2020-04-15 02:34:01 +0200

    :whale: Improve wait for DB
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,83 @@
 #!/bin/sh
 set -e
 
-echo "Application entrypoint initialization..."
+###########################################################
+# Functions
+
+log() {
+    MESSAGE=$1
+
+    echo "[$0] [$(date +%Y-%m-%dT%H:%M:%S)] ${MESSAGE}"
+}
+
+wait_for_host() {
+    WAIT_FOR_ADDR=${1}
+    if [ -z "${WAIT_FOR_ADDR}" ]; then
+        log "Missing host's address to wait for!"
+        exit 1
+    fi
+
+    WAIT_FOR_PORT=${2}
+    if [ -z "${WAIT_FOR_PORT}" ]; then
+        log "Missing host's port to wait for!"
+        exit 1
+    fi
+
+    WAIT_TIME=0
+    WAIT_STEP=${3:-10}
+    WAIT_TIMEOUT=${4:--1}
+
+    while ! nc -z "${WAIT_FOR_ADDR}" "${WAIT_FOR_PORT}" ; do
+        if [ "${WAIT_TIMEOUT}" -gt 0 ] && [ "${WAIT_TIME}" -gt "${WAIT_TIMEOUT}" ]; then
+            log "Host '${WAIT_FOR_ADDR}:${WAIT_FOR_PORT}' was not available on time!"
+            exit 1
+        fi
+
+        log "Waiting host '${WAIT_FOR_ADDR}:${WAIT_FOR_PORT}'..."
+        sleep "${WAIT_STEP}"
+        WAIT_TIME=$(( WAIT_TIME + WAIT_STEP ))
+    done
+    log "Host '${WAIT_FOR_ADDR}:${WAIT_FOR_PORT}' available."
+}
+
+wait_for_hosts() {
+    WAIT_FOR_HOSTS=${1}
+    if [ -z "${WAIT_FOR_HOSTS}" ]; then
+        log "Missing hosts to wait for!"
+        exit 1
+    fi
+
+    for H in ${WAIT_FOR_HOSTS}; do
+        WAIT_FOR_ADDR=$(echo "${H}" | cut -d: -f1)
+        WAIT_FOR_PORT=$(echo "${H}" | cut -d: -f2)
+
+        wait_for_host "${WAIT_FOR_ADDR}" "${WAIT_FOR_PORT}" "${WAIT_STEP}" "${WAIT_TIMEOUT}"
+    done
+
+}
+
+# date_greater A B returns whether A > B
+date_greater() {
+    [ $(date -u -d "$1" -D "%Y-%m-%dT%H:%M:%SZ" +%s) -gt $(date -u -d "$2" -D "%Y-%m-%dT%H:%M:%SZ" +%s) ];
+}
+
+
+###########################################################
+# Wait for it...
+
+if [ -n "${WAIT_FOR}" ]; then
+    wait_for_hosts "${WAIT_FOR}" "${WAIT_STEP}" "${WAIT_TIMEOUT}"
+fi
+
+
+###########################################################
+# Runtime
+
+log "Application entrypoint initialization..."
 
 # If no config provided in volume, setup a default config from environment variables
 if [ ! -f "${APP_CONFIG}" ]; then
-	echo "Setting up initial application configuration..."
+	log "Setting up initial application configuration..."
 	echo "# Initial configuration generated at $(date +%Y-%m-%dT%H:%M:%S%z)" > ${APP_CONFIG}
 
 	echo "# ~~~~~" >> "${APP_CONFIG}"
@@ -36,32 +108,35 @@ if [ ! -f "${APP_CONFIG}" ]; then
 	if [ ! -z "${APP_SIGNING_KEYPAIR_PASS}" ]; then
 		mkdir -p /srv/app/keys
 
-		echo "Generating RSA Java Key Store for verifying access tokens..."
+		log "Generating RSA Java Key Store for verifying access tokens..."
 		keytool -genkeypair -alias "${APP_SIGNING_KEYPAIR_ALIAS}" -dname "CN=${APP_DOMAIN_NAME}, OU=Unknown, O=Unknown, L=Unknown, S=Unknown, C=Unknown" -keystore /srv/app/keys/.private.jks -keyalg RSA -keypass "$APP_SIGNING_KEYPAIR_PASS" -storepass "$APP_SIGNING_KEYPAIR_PASS"
 
 		keytool -importkeystore -srckeystore /srv/app/keys/.private.jks -srcstorepass "${APP_SIGNING_KEYPAIR_PASS}" -destkeystore /srv/app/keys/.private.jks -deststoretype pkcs12
 		chmod 600 /srv/app/keys/.private.jks
-		echo "RSA JKS generated in /srv/app/keys/"
+		log "RSA JKS generated in /srv/app/keys/"
 
 		keytool -list -rfc --keystore /srv/app/keys/.private.jks -storepass "${APP_SIGNING_KEYPAIR_PASS}" | openssl x509 -inform pem -pubkey -noout > /srv/app/keys/public.txt
-		echo "Public key extracted in /srv/app/keys/"
+		log "Public key extracted in /srv/app/keys/"
 
 		echo "# Access token signing key" >> "${APP_CONFIG}"
 		echo "application.security.key-pair-path=/srv/app/keys/.private.jks" >> "${APP_CONFIG}"
 		echo "application.security.key-pair-password=${APP_SIGNING_KEYPAIR_PASS}" >> "${APP_CONFIG}"
 		echo "application.security.key-pair-alias=${APP_SIGNING_KEYPAIR_ALIAS}" >> "${APP_CONFIG}"
 		echo "application.security.verifying-key-path=/srv/app/keys/public.txt" >> "${APP_CONFIG}"
+		log "RSA JKS configured"
 	elif [ -z "${APP_SIGNING_KEY}" ]; then
 		mkdir -p /srv/app/keys
 
-		echo "Generating RSA SSH key for signing access tokens..."
+		log "Generating RSA SSH key for signing access tokens..."
 		ssh-keygen -b 2048 -m PEM -t rsa -f /srv/app/keys/.id_rsa -q -N ""
-		echo "RSA SSH key generated in /srv/app/keys/"
+		log "RSA SSH key generated in /srv/app/keys/"
 
 		echo "# Access token signing key" >> "${APP_CONFIG}"
 		echo "application.security.signing-key-path=/srv/app/keys/.id_rsa.pub" >> "${APP_CONFIG}"
 		echo "application.security.verifying-key-path=/srv/app/keys/.id_rsa" >> "${APP_CONFIG}"
+		log "RSA SSH key configured"
 	else
+		log "Setting signing key (not recommended for production)..."
 		echo "# Access token signing key" >> "${APP_CONFIG}"
 		echo "application.security.signing-key=${APP_SIGNING_KEY}" >> "${APP_CONFIG}"
 	fi
@@ -125,7 +200,7 @@ if [ ! -f "${APP_CONFIG}" ]; then
 	elif [ "${DB_PLATFORM}" = 'h2' ]; then
 		mkdir -p /srv/app/data/h2
 
-		echo "In memory H2 database will be stored in /srv/app/data/h2"
+		log "In memory H2 database will be stored in /srv/app/data/h2"
 		echo "spring.datasource.url=jdbc:h2:file:/srv/app/data/h2/${DB_NAME}" >> "${APP_CONFIG}"
 	else
 		echo "spring.datasource.url=jdbc:${DB_PLATFORM}://localhost:${DB_PORT}/${DB_NAME}" >> "${APP_CONFIG}"
@@ -177,16 +252,10 @@ if [ ! -f "${APP_CONFIG}" ]; then
 	#echo "# if set, create groups on ldap server under ldap.group.base" >> "${APP_CONFIG}"
 	#echo "ldap.group.object.class=${LDAP_GROUP_CLASS}" >> "${APP_CONFIG}"
 
-	echo "Configuration generated."
+	log "Configuration generated."
 else
-	echo "Configuration found."
+	log "Configuration found."
 fi
 
-# FIXME kind of ugly... should wait dynamically until DB is up
-if [ ! "${DB_PLATFORM}" = 'h2' ]; then
-	echo "Wait until all containers finished initializing themselves..."
-	sleep 240
-fi
-
-echo "Launching application..."
+log "Launching application..."
 exec "$@"
